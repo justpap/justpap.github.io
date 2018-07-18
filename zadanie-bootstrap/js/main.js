bafd9617636c47a1f6fbcd7b7dc3212bf0d85b0c
Justyna Paprzycka <justynaciszek7@gmail.com>
2018-07-18 22:29:15 +0200

    zadanie domowe bootsrtrap

---
+++ zadanie-bootstrap/js/main.js
@@ -0,0 +1,25 @@
+//const links = $("a:not([href='#'])");
+//const body = $("body");
+//const scrollTime = 600;
+//
+//function navigation(e) {
+    'use strict';
+//    'use strict';
+//    e.preventDefault();
+//    links.removeClass('active');
+//    $(this).addClass("active");
+//
+//    let targetElement = $(this.hash);
+//    $("html, body").animate({scrollTop: targetElement.offset().top},scrollTime);
+//}
+//
+//function scrollWindow() {
+//    if( $(window).scrollTop() > 20 ) {
+//        body.addClass("move");
+//    } else {
+//        body.removeClass("move");
+//    }
+//}
+
+//links.on("click", navigation);
+//$(window).on("scroll", scrollWindow);
